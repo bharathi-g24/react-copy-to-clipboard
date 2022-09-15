a684fa852ab92cd1a0626bc9483df7fba94c5b43
bharathi-g24 <113513781+bharathi-g24@users.noreply.github.com>
2022-09-15 11:45:58 +0530

    export copy for handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 const {CopyToClipboard} = require('./Component');
+const copy = require('copy-to-clipboard');
 
 
 CopyToClipboard.CopyToClipboard = CopyToClipboard;
-module.exports = CopyToClipboard;
+module.exports = {
+  CopyToClipboard,
+  copy
+};
